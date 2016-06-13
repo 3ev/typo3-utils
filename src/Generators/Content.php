0c58cc1986ec4799f627cb033dc0ced26712c03e
Ben Constable <ben@benconstable.co.uk>
2016-06-13 12:14:06 +0100

    Add flex form config options to plugin generation

--- src/Generators/Content.php
+++ src/Generators/Content.php
@@ -32,6 +32,15 @@ class Content extends Base
     /**
      * Create a plugin element.
      *
+     * Simplifies the creation of the associated FlexForm by allowing you to pass
+     * $attrs['form'], containing a of sheet names to flex form config. For example:
+     *
+     * $attrs['form'] => [
+     *     'Sheet1' => [
+     *         'settings.title' => 'An Example'
+     *     ]
+     * ]
+     *
      * @param  int  $pid
      * @param  string  $plugin  Plugin name, of the form vendorextension_pluginname
      * @param  array  $attrs
@@ -39,6 +48,32 @@ class Content extends Base
      */
     public function createPlugin($pid, $plugin, $attrs = [])
     {
+        if (isset($attrs['form'])) {
+            $flexFormSheets = [];
+
+            foreach ($attrs['form'] as $sheet => $setup) {
+                $flexFormSheets[$sheet] = [];
+
+                foreach ($setup as $key => $value) {
+                    $flexFormSheets[$sheet][$key] = [
+                        'vDEF' => $value
+                    ];
+                }
+            }
+
+            $attrs['pi_flexform'] = [
+                'data' => []
+            ];
+
+            foreach ($flexFormSheets as $sheet => $setup) {
+                $attrs['pi_flexform']['data'][$sheet] = [
+                    'lDEF' => $setup
+                ];
+            }
+
+            unset($attrs['form']);
+        }
+
         return $this->create($pid, array_merge($attrs, [
             'CType' => 'list',
             'list_type' => $plugin
