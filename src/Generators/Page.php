faac1b340aeef7080df0f0db2d5fef57b6dcbff1
Ben Constable <ben@benconstable.co.uk>
2016-06-16 09:12:48 +0100

    Allow Fluid Page flexforms to be generated for pages

--- src/Generators/Page.php
+++ src/Generators/Page.php
@@ -16,6 +16,57 @@ class Page extends Base
     }
 
     /**
+     * Create a page.
+     *
+     * You may optionally set the Fluid Pages extension flexform by passing a 'form'
+     * hash, which is a set of sheets and their fields.
+     *
+     * The default sheet name is 'page', so you'll typically be using that.
+     *
+     * [
+     *     'form' => [
+     *         'page' => [
+     *             'settings.myfield' => 'An example'
+     *         ]
+     *     ]
+     * ]
+     *
+     * @param  int  $pid  Parent PID
+     * @param  array  $attrs  Page attributes
+     * @return   int|null  Created page UID or null if failed
+     */
+    public function create($pid, $attrs = [])
+    {
+        if (isset($attrs['form'])) {
+            $flexFormSheets = [];
+
+            foreach ($attrs['form'] as $sheet => $setup) {
+                $flexFormSheets[$sheet] = [];
+
+                foreach ($setup as $key => $value) {
+                    $flexFormSheets[$sheet][$key] = [
+                        'vDEF' => $value
+                    ];
+                }
+            }
+
+            $attrs['tx_fed_page_flexform'] = [
+                'data' => []
+            ];
+
+            foreach ($flexFormSheets as $sheet => $setup) {
+                $attrs['tx_fed_page_flexform']['data'][$sheet] = [
+                    'lDEF' => $setup
+                ];
+            }
+
+            unset($attrs['form']);
+        }
+
+        return parent::create($pid, $attrs);
+    }
+
+    /**
      * Create a storage folder.
      *
      * @param  int  $pid
